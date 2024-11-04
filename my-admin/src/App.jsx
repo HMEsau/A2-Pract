7b4d178e3d60c704cf72f04b7abb117c24341fd0
HM Esau <140419559+HMEsau@users.noreply.github.com>
2024-11-04 09:24:46 +0000

    added

--- my-admin/src/App.jsx
+++ my-admin/src/App.jsx
@@ -1,35 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Admin, Resource} from 'react-admin';
+import {Dataprovider} from '@react-admin/ra-data-pocketbase';
+import {List, Datagrid, TextField} from 'react-admin';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => (
+  <Admin dataProvider={Dataprovider('http://localhost:8090')}>
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate}
+  </Admin>
+);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
